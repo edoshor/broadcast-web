dd215c090a1e5c45ff18a2613f0c50516d4b7c99
Edo Shor <edoshor@gmail.com>
2013-05-09 19:39:00 +0300

    compile all controller js assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ BroadcastWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css.scss, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( transcoder_test.js, transcoder_console.js )
+  config.assets.precompile += %w( transcoder_test.js, 
+  transcoder_console.js, transcoders.js, sources.js, presets.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
