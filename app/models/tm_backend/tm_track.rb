5ec8409a09368e7f68df35783948bd34e902ccb1
Edo Shor <edoshor@gmail.com>
2014-11-14 03:23:44 +0200

    Add new profiles to human readable descriptions

--- app/models/tm_backend/tm_track.rb
+++ app/models/tm_backend/tm_track.rb
@@ -17,13 +17,21 @@ class TMTrack
   def human_video_profile
     case profile_number
       when 1
-        '320x240, 120Kbit'
+        '320x240, 120Kbit, 4:3'
       when 2
-        '360x270, 200Kbit'
+        '360x270, 200Kbit, 4:3'
       when 3
-        '640x480, 400Kbit'
+        '640x480, 400Kbit, 4:3'
       when 4
-        '720x540, 600Kbit'
+        '720x540, 600Kbit, 4:3'
+      when 11
+        '320x180, 120Kbit, 16:9'
+      when 12
+        '360x202, 200Kbit, 16:9'
+      when 13
+        '640x360, 400Kbit, 16:9'
+      when 14
+        '720x404, 600Kbit, 16:9'
       else
         profile_number
     end
